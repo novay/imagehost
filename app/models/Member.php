f8e7a9dc7548143aff99bfe45f02cd88cc2d20b6
Novay Mawbowo <novay@otaku.si>
2014-02-14 17:27:54 +0800

    Fix Model Member

--- app/models/Member.php
+++ app/models/Member.php
@@ -20,4 +20,41 @@ class Member extends Eloquent implements UserInterface, RemindableInterface {
 		'password' => 'required|min:5',
 	];
 
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = array('password');
+
+	/**
+	 * Get the unique identifier for the user.
+	 *
+	 * @return mixed
+	 */
+	public function getAuthIdentifier()
+	{
+		return $this->getKey();
+	}
+
+	/**
+	 * Get the password for the user.
+	 *
+	 * @return string
+	 */
+	public function getAuthPassword()
+	{
+		return $this->password;
+	}
+
+	/**
+	 * Get the e-mail address where password reminders are sent.
+	 *
+	 * @return string
+	 */
+	public function getReminderEmail()
+	{
+		return $this->email;
+	}
+
 }
