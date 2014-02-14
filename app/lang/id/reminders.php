ff3a49040f1571fbc6e3b558528c5e2b3ac7bc1b
Novay Mawbowo <novay@otaku.si>
2014-02-14 16:17:59 +0800

    Validasi Indonesia

---
+++ app/lang/id/reminders.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Kata Sandi minimal 6 karakter dan harus dikonfirmasi.",
+
+	"user" => "Pengguna dengan email tersebut tidak ditemukan.",
+
+	"token" => "Token untuk reset sandi tidak cocok..",
+
+	"sent" => "Kata Sandi pengingat telah terkirim!",
+
+);
