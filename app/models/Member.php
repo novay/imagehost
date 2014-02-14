96b09999a9db0bbdeff3fbb04ba4053bc8d1a746
Novay Mawbowo <novay@otaku.si>
2014-02-14 17:13:05 +0800

    Isi Folder Public

--- app/models/Member.php
+++ app/models/Member.php
@@ -1,7 +1,23 @@
 <?php
 
-class Member extends Eloquent {
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableInterface;
 
-	// 
+class Member extends Eloquent implements UserInterface, RemindableInterface {
+
+	# Ambil tabel dari database
+	protected $table = 'member';
+
+	# Field yang boleh di input
+	protected $fillable = [''];
+
+	# Field yang jadi patokan
+	protected $guarded = ['id'];
+
+	# Rules validasi
+	public static $rules = [
+		'username' => 'required|min:5|max:20|exists:member,username', 
+		'password' => 'required|min:5',
+	];
 
 }
